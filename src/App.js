b49cfbf032722b21a5fb1c7143e80a5eca583237
Nitish Phanse <nitish.phanse@myntra.com>
2021-07-16 00:24:34 +0530

    added basic table

--- src/App.js
+++ src/App.js
@@ -1,128 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import users from "./data.json"
 import './App.css';
+const filteredLocations = users.results.map(({location}) => location)
 
-import SortableTable from './SortableTable';
+/**
+ * 
+ * Create a Table Component And move the Table logic to the New Component
+ * Pass data to the component as props
+ */
 
-
-const SortingDirection = {
-  ASCENDING: 'ASCENDING',
-  DESCENDING: 'DESCENDING',
-  UNSORTED: 'UNSORTED',
-};
-
-const fetchData = () => {
-  return axios
-    .get('http://localhost:8989/users')
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => console.error('error occured', err));
-};
-
-const flattenLocation = (location) => {
-  const data = [];
-  for (const { street, timezone, coordinates, ...rest } of location) {
-    data.push({
-      ...rest,
-      number: street.number,
-      name: street.name,
-      latitude: coordinates.latitude,
-      longitude: coordinates.longitude,
-    });
-  }
-  const getAllKeys = getAllHeaders(data[0]);
-  return { headers: getAllKeys, data };
-  // console.log('the result is ', result);
-};
-
-const getFilterdRows = (rows, filterKey) => {
-  return rows.filter((row) => {
-    return Object.values(row).some((s) =>
-      (' ' + s).toLowerCase().includes(filterKey)
-    );
-  });
-};
-
-const getAllHeaders = (data) => {
-  console.log(' Object.keys', Object.keys(data));
-  return Object.keys(data);
-  // return Object.keys(data).reduce((key, value) => {});
-};
-
-const sortData = (dataToBeSorted, sortKey, direction) => {
-  dataToBeSorted.sort((a, b) => {
-    const relA = a[sortKey];
-    const relB = b[sortKey];
-    if (
-      direction === SortingDirection.UNSORTED ||
-      direction === SortingDirection.ASCENDING
-    ) {
-      if (relA < relB) return -1;
-      if (relA > relB) return 1;
-      return 0;
-    } else {
-      if (relA > relB) return -1;
-      if (relA < relB) return 1;
-      return 0;
-    }
-  });
-};
-
-const getNextSortingDirection = (sortingDirection) => {
-  if (
-    sortingDirection === SortingDirection.UNSORTED ||
-    sortingDirection === SortingDirection.ASCENDING
-  ) {
-    return SortingDirection.DESCENDING;
-  }
-  return SortingDirection.ASCENDING;
-};
 
 function App() {
-  // const [people, setPeople] = useState([]);
-  const [location, setAllLocation] = useState({
-    headers: [],
-    data: [],
-  });
-
-  const [sortingDireection, setSortingDirection] = useState({});
-
-  const sortColumn = (sortKey) => {
-    const newFlattenedList = {
-      ...location,
-      data: [...location.data],
-    };
-    const currentSortingDirection = sortingDireection[sortKey];
-    sortData(newFlattenedList.data, sortKey, currentSortingDirection);
-    const nextSortingDirection = getNextSortingDirection(
-      currentSortingDirection
-    );
-
-    const newSortingDirection = { ...sortingDireection };
-    newSortingDirection[sortKey] = nextSortingDirection;
-
-    setAllLocation(newFlattenedList);
-    setSortingDirection(newSortingDirection);
-  };
-
-  useEffect(() => {
-    fetchData().then((eachResults) => {
-      const ourFlatteredLocation = flattenLocation(
-        eachResults.map(({ location }) => location)
-      );
-      setAllLocation(ourFlatteredLocation);
-      console.log('the flattened Array is ', ourFlatteredLocation);
-    });
-  }, []);
+  const [locations, setAllLocation] = useState(filteredLocations);
   return (
     <div className="App">
       <h1> Lets Start Coding</h1>
-      <SortableTable
-        headers={location.headers}
-        tableData={getFilterdRows(location.data, '')}
-        sortColumn={sortColumn}
-      />
+      <table>
+        
+        <tbody>
+          {locations.map((eachLoc, eachLocIndex) => (
+            <tr key={eachLocIndex}>
+              <td>{eachLoc.city}</td>
+              <td>{eachLoc.state}</td>
+              <td>{eachLoc.postcode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
     </div>
   );
 }
