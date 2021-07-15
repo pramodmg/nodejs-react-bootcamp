85c77fa1b30185373e7c6e27f464d77b41eac21d
Nitish Phanse <nitish.phanse@myntra.com>
2021-07-16 00:38:55 +0530

    added helper component for flatten

--- src/App.js
+++ src/App.js
@@ -2,28 +2,64 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import users from "./data.json"
 import './App.css';
-const filteredLocations = users.results.map(({location}) => location)
+import { restElement } from '@babel/types';
 
+
+const filteredLocations = users.results.map(({location}) => {
+  const { street, timezone, coordinates, ...rest } = location;
+  return {
+    ...rest,
+    number: street.number,
+    name: street.name,
+    latitude: coordinates.latitude,
+    longitude: coordinates.longitude,
+  }
+})
+
+const getAllHeaders = (data) => {
+  console.log(' Object.keys', Object.keys(data));
+  return Object.keys(data);
+  // return Object.keys(data).reduce((key, value) => {});
+};
 /**
  * 
  * Create a Table Component And move the Table logic to the New Component
  * Pass data to the component as props
+ * Pass the sort column as a prop to the new table
  */
 
 
 function App() {
-  const [locations, setAllLocation] = useState(filteredLocations);
+  const allHeaders = getAllHeaders(filteredLocations[0])
+  const [locations, setAllLocation] = useState({
+    headers: allHeaders,
+    data: filteredLocations,
+  });
+
+  const sortColumn = (sortKey) => {
+    console.log(sortKey)
+  }
+
+  
   return (
     <div className="App">
       <h1> Lets Start Coding</h1>
       <table>
-        
+      <thead>
+          <tr>
+            {locations.headers.map((each, eachKey) => (
+              <th key={eachKey} onClick={() => sortColumn(each)}>
+                {each}
+              </th>
+            ))}
+          </tr>
+        </thead>
         <tbody>
-          {locations.map((eachLoc, eachLocIndex) => (
+          {locations.data.map((eachLoc, eachLocIndex) => (
             <tr key={eachLocIndex}>
-              <td>{eachLoc.city}</td>
-              <td>{eachLoc.state}</td>
-              <td>{eachLoc.postcode}</td>
+              {locations.headers.map((header, headerInd) => (
+                <td key={headerInd}>{eachLoc[header]}</td>
+              ))}
             </tr>
           ))}
         </tbody>
