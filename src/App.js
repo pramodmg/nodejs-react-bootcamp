5cb4927d99a6e6d1fd20515eb66244e01c9d561e
Nitish Phanse <nitish.phanse@myntra.com>
2021-07-16 10:25:31 +0530

    removed deleted code

--- src/App.js
+++ src/App.js
@@ -38,14 +38,6 @@ const flattenLocation = (location) => {
   // console.log('the result is ', result);
 };
 
-const getFilterdRows = (rows, filterKey) => {
-  return rows.filter((row) => {
-    return Object.values(row).some((s) =>
-      (' ' + s).toLowerCase().includes(filterKey)
-    );
-  });
-};
-
 const getAllHeaders = (data) => {
   console.log(' Object.keys', Object.keys(data));
   return Object.keys(data);
@@ -116,7 +108,7 @@ function App() {
       <h1> Lets Start Coding</h1>
       <SortableTable
         headers={location.headers}
-        tableData={getFilterdRows(location.data, '')}
+        tableData={location.data}
         sortColumn={sortColumn}
       />
     </div>
